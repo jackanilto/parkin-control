4e5a5a06ad327d52f09ae2442a173a24bcd9d4de
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-09-02 23:15:33 -0300

    Criando metodo que recebe a solicitação Raiz

--- src/main/java/com/api/parkincontrol/ParkinControlApplication.java
+++ src/main/java/com/api/parkincontrol/ParkinControlApplication.java
@@ -2,12 +2,19 @@ package com.api.parkincontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController /*Bean do tipo controller*/
 public class ParkinControlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkinControlApplication.class, args);
 	}
 
+	@GetMapping("/") /*Define a raiz do projeto*/
+	public String index(){
+		return "Olá Mundo!";
+	}
 }
